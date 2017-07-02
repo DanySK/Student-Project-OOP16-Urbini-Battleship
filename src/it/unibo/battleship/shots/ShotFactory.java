7ac9e3e43782ef2414fcfd16d05ab3168ba94601
Fabio Urbini <fabio.urbini@studio.unibo.it>
2017-07-02 13:23:11 +0200

    Reviewed notes

--- src/it/unibo/battleship/shots/ShotFactory.java
+++ src/it/unibo/battleship/shots/ShotFactory.java
@@ -6,6 +6,7 @@ import java.io.Serializable;
  * Represents a Shot Factory
  * @author fabio.urbini
  */
+@FunctionalInterface
 public interface ShotFactory extends Serializable {
   Shot createShot();
 }
